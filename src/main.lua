9f8e3afbd820fc5b33a94101ad8192f0f277b112
Cacilhας, La Batalema <batalema@cacilhas.info>
2014-11-23 11:19:39 -0200

    use Lua style guide
    http://lua-users.org/wiki/LuaStyleGuide

--- src/main.lua
+++ src/main.lua
@@ -4,41 +4,41 @@ local app = {}
 
 ------------------------------------------------------------------------
 function reset()
-    app.board = tictactoe.newboard(app)
-    app.current, app.next = "x", "o"
+  app.board = tictactoe.newboard(app)
+  app.current, app.next = "x", "o"
 end
 
 
 ------------------------------------------------------------------------
 function love.load()
-    app.background = love.graphics.newImage("images/background.png")
-    app.foreground = love.graphics.newImage("images/xo.png")
-    app.tie = love.graphics.newImage("images/tie.png")
-    app.xquad = love.graphics.newQuad(0, 0, 174, 186, 348, 186)
-    app.oquad = love.graphics.newQuad(174, 0, 348, 186, 348, 186)
-    reset()
+  app.background = love.graphics.newImage("images/background.png")
+  app.foreground = love.graphics.newImage("images/xo.png")
+  app.tie = love.graphics.newImage("images/tie.png")
+  app.xquad = love.graphics.newQuad(0, 0, 174, 186, 348, 186)
+  app.oquad = love.graphics.newQuad(174, 0, 348, 186, 348, 186)
+  reset()
 end
 
 
 ------------------------------------------------------------------------
 function love.draw()
-    app.board:draw()
+  app.board:draw()
 end
 
 
 ------------------------------------------------------------------------
 function love.keyreleased(key)
-    if key == "escape" then reset() end
+  if key == "escape" then reset() end
 end
 
 
 ------------------------------------------------------------------------
 function love.mousereleased(x, y, button)
-    if button == "l" then
-        local rx = math.floor(x / 200) + 1
-        local ry = math.floor(y / 210) + 1
-        if app.board:toggle(app.current, rx, ry) then
-            app.current, app.next = app.next, app.current
-        end
+  if button == "l" then
+    local rx = math.floor(x / 200) + 1
+    local ry = math.floor(y / 210) + 1
+    if app.board:toggle(app.current, rx, ry) then
+      app.current, app.next = app.next, app.current
     end
+  end
 end
